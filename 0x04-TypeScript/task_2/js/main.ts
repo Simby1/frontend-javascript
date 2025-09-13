55aec98e96cb97410e8d5b280771b574a58e7076
Simby1 <kimsimby@gmail.com>
2025-09-13 21:37:37 +0100

    `Deleted files and directories for 0x00-semantic_html, 0x02-tailwind-css, 0x03-sass_scss, and 0x04-TypeScript tasks`

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,7 +62,7 @@ function executeWork(employee: Director | Teacher): void {
 // String literal types
 type Subjects = 'Math' | 'History';
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
   }
